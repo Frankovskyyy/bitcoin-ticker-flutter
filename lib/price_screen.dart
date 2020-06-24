029b4ebad7a3d65ea6bdcefe15cd412dabbbaaf8
Frankovskyyy <62710663+Frankovskyyy@users.noreply.github.com>
2020-06-24 10:21:21 +0200

    Błąd-dropdown

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +44,18 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+                items: [
+                  DropdownMenuItem(child: Text('USD'), value: 'USD'),
+                  DropdownMenuItem(child: Text('EUR'), value: 'EUR'),
+                  DropdownMenuItem(child: Text('GBP'), value: 'GBP'),
+                ],
+                value: selectedCurrency, //BŁĄDDDDDDDDDDDDDDDDDDDDDDDDDDD
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value;
+                  });
+                }),
           ),
         ],
       ),
